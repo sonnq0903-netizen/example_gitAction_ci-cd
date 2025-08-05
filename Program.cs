ba7efa527e4b54f1e65b848778719502057daa85
Quang Son <sonnq0903@gmail.com>
2025-08-05 10:50:44 +0700

    fix: dieu chinh  port program

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 // Đọc cấu hình Kestrel từ appsettings
-builder.WebHost.ConfigureKestrel(options =>
-{
-    var kestrelConfig = builder.Configuration.GetSection("Kestrel");
-    if (kestrelConfig.Exists())
-    {
-        options.Configure(kestrelConfig);
-    }
-});
+// builder.WebHost.ConfigureKestrel(options =>
+// {
+//     var kestrelConfig = builder.Configuration.GetSection("Kestrel");
+//     if (kestrelConfig.Exists())
+//     {
+//         options.Configure(kestrelConfig);
+//     }
+// });
+builder.WebHost.UseUrls("http://0.0.0.0:5158");
+
 
 var app = builder.Build();
 
